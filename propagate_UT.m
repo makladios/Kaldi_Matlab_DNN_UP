053de00de17b2a88cdfe2c44836d6b3e576ee484
makladios <a.m.serag@gmail.com>
2015-02-20 10:33:23 +0100

    modified to accept an entire utterance

--- propagate_UT.m
+++ propagate_UT.m
@@ -22,27 +22,26 @@ function [Mu_out,Cov_out]=propagate_UT(Mu_in,Cov_in)
 
 n=1;
 k=2;
-d=length(Mu_in);
+[d,T]=size(Mu_in);
 
 % Choose 3 points
-x=zeros(d,2*n+1);
-x(:,1)=Mu_in;
+x=zeros(d,T,2*n+1);
+x(:,:,1)=Mu_in;
 SQ = sqrt((n+k)*Cov_in); 
-x(:,2)=Mu_in+SQ;
-x(:,3)=Mu_in-SQ;
+x(:,:,2)=Mu_in+SQ;
+x(:,:,3)=Mu_in-SQ;
 
 % Transfor the points by applying the sigmoid function
 y=sigmoid(x);
 
 % Compute the weights
-w(:,1)=k/(n+k)*ones(d,1);
-w(:,2:2*n+1)=1/(2*(n+k))*ones(d,2);
+w(:,:,1)=k/(n+k)*ones(d,T);
+w(:,:,2:2*n+1)=1/(2*(n+k))*ones(d,T,2*n);
 
 % Compute the mean
-Mu_out=sum(y.*w,2);
+Mu_out=sum(y.*w,3);
 
 % Compute the Covariance
-MuFreeY=y-repmat(Mu_out,[1,size(y,2)]);
-Cov_out=sum(w.*(MuFreeY.^2),2);
- 
+MuFreeY=y-repmat(Mu_out,[1,1,2*n+1]);
+Cov_out=sum(w.*(MuFreeY.^2),3); 
  
